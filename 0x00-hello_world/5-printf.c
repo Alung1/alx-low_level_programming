a493f180feff3d08c030f0a6a4c1d7805e19d0c7
Michael Akparibo Alung <alungm1@gmail.com>
2022-10-13 17:58:24 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main-serves as the entry point for the program
+ *
+ *Return: By default returns zero if no error,
+ *otherwise returns non-zero value.
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
