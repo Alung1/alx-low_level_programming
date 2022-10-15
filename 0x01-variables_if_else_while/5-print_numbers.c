a39f4dea7af6e93836d8abaaebb03581a7e0b1ac
Michael Akparibo Alung <alungm1@gmail.com>
2022-10-14 17:04:34 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main-program entry point.
+ *Return:0 if no error,else return non zero
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+{
+	putchar(i);
+}
+
+	putchar('\n');
+	return (0);
+}
