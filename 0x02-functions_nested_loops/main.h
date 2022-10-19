0d758de9bf2d0f2137035ac40dc730e4d07d9d37
vagrant <alungm1@gmail.com>
2022-10-19 11:45:53 +0000

    main head modified

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef main_h
+#define main_h
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -6,4 +8,6 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+void jack_bauer(void);
 void times_table(void);
+#endif
