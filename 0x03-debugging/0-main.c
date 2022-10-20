9fdbd64b89a431e21debfa08ad537ed429467334
vagrant <alungm1@gmail.com>
2022-10-20 16:23:29 +0000

    fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,20 +7,10 @@
  */
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n >  0)
-{
-	printf("%d is positive\n", n);
-}
-	else if (n < 0)
+	i = 0;
+	positive_or_negative(i);
 
-{
-	printf("%d is negative\n", n);
-}
-	else if (n == 0)
-	printf("%d is zero\n", n);
 	return (0);
 }
