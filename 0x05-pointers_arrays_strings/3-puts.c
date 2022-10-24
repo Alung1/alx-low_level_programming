60d4085974cf0980fe619f66efdae7f29e47a1f1
Michael Akparibo Alung <alungm1@gmail.com>
2022-10-24 13:52:10 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  *_puts -> prints a string
- *@str: string
- *Return: Length
+ *@str: the string
+ *Return: the length ofthe string
  */
 
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (*str[i] != '\0')
+	while (str[i] != '\0')
 	{
-		_putchar(*str[i]);
+		_putchar(str[i]);
 		i++;
 	}
 	_putchar('\n');
