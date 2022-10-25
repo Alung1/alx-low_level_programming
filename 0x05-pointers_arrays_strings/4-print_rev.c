a53b9c53a4940f788bf40bc1623c30432333581d
vagrant <alungm1@gmail.com>
2022-10-25 16:31:12 +0000

    modify task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,13 @@
 
 void print_rev(char *s)
 {
-	int m = 0
+	int i = 0
 
-	while (s[m] != '\0')
-		m++;
-	for (m = m - 1; m >= 0; m--)
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
 	{
-	_putchar(s[m]);
+	_putchar(s[i]);
 	}
 	_putchar('\n');
 }
