e0822253b0959e69d62fc66c1afb8cf5794b1118
vagrant <alungm1@gmail.com>
2022-10-26 20:07:31 +0000

    tasks

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*
+*
+*
+*
+*/
+
+char *_strcat(char *dest, char *src)
+
+{
+	char *s =dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	
+	while (*src 
+
+
+}
