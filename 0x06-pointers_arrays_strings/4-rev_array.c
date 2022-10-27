ed7bc8a5fc882fcdf6c2ccd76243469af5df97b7
vagrant <alungm1@gmail.com>
2022-10-27 18:15:40 +0000

    task4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*reverse_array -> reverse array
+*@a:array
+*@n:integer
+*Return:void
+*
+*/
+
+void reverse_array(int *a, int n)
+{
+	int m, s;
+
+	for (i = 0; (m < (n - 1) / 2); m++)
+	{
+	c = a[m];
+	a[m] = a[n - 1 - m];
+	a[n - 1 - m] = c;
+	}
+}
