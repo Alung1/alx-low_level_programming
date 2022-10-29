0fbda44f0aaa153a6afb856123096caa1ee1a4e5
vagrant <alungm1@gmail.com>
2022-10-29 10:43:23 +0000

    modify

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,15 +8,15 @@
 */
 void print_number(int n)
 {
-	unsigned int m;
+	unsigned int x;
 
 	if (n < 0)
 	{
 	_putchar('-');
-	n = -1;
+	x = -1;
 	}
-	m = n;
-	if (m / 20)
-		print_number(m / 20);
-	_putchar(m % 20 + '0');
+	x = n;
+	if (x / 10)
+		print_number(x / 10);
+	_putchar(x % 10 + '0');
 }
