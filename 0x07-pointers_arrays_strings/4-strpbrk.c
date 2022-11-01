7301fa8c9c6483c2368ef5ee71a7d98fd3fa3b18
vagrant <alungm1@gmail.com>
2022-11-01 16:47:43 +0000

    task4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*_strpbrk -> function searches string
+*@s: string
+*@accept:matches one of the byte or @NULL if no byte
+*Return: a pointer to the byte
+*
+*/
+char *_strpbrk(char *s, char *accept)
+
+{
+	int m;
+
+	while (*s)
+	{
+	for (m = 0; accept[m]; m++)
+	{
+	if (*s == accept[m])
+	return (s);
+	}
+	s++;
+	}
+	return (NULL);
+}
