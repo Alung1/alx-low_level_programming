266e8b2e4b6eb5a7ed79cd12b357fa0fc953b3b5
vagrant <alungm1@gmail.com>
2022-11-09 21:08:53 +0000

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -a program that adds positive numbers.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int m, s, sum = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (m = 1; argv[m]; i++)
+	{
+		s = strtol(argv[m], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += s;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
