43721c9d8f5fd9b23a37024df921f70a24e14cc8
vagrant <alungm1@gmail.com>
2022-11-12 11:15:27 +0000

    task1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * malloc_checked -> cause normal process termination with a status value of 98
  * @b: allocated memory
@@ -12,7 +12,7 @@ void *malloc_checked(unsigned int b)
 
 	i = malloc(b);
 	if (i == NULL)
-	{	
+	{
 	exit(98);
 	}
 
