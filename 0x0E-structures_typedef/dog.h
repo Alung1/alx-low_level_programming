aa45c78c634fcfa2166cd780ffb9c9b55e2a17cc
vagrant <alungm1@gmail.com>
2022-11-15 19:42:18 +0000

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog -> dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_m;
+
+dog_m *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_m *d);
+
+#endif
