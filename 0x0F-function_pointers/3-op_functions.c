3a295661062c422f883aeb5c907b22b9ab3d6a63
vagrant <alungm1@gmail.com>
2022-11-19 09:12:04 +0000

    task3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,75 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * op_add -> sum of a and b
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: ...
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub -> returns the difference 
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: ...
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul -> returns product of.
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: ...
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div -> results of the division
+  * @a: first int
+  * @b: second int
+  *
+  * Return: ...
+  */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+  * op_mod -> returns remainder of.
+  * @a: first int
+  * @b: second int
+  *
+  * Return: ...
+  */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
