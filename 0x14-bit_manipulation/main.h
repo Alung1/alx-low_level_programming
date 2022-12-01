d74b9822458a64de9d9fd225dee8bb50c871d6b7
vagrant <alungm1@gmail.com>
2022-12-01 17:48:14 +0000

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-
+int _putchar (char c);
+unsigned long int _power(unsigned int base, unsigned int pow);
 #endif
